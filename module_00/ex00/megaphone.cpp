9489ee1909a598e047b379f3df5b8ade60b516f4
ahmetuz <pro.ahmetuzn@gmail.com>
2023-01-02 02:03:44 +0100

    megaphone -> done

--- module_00/ex00/megaphone.cpp
+++ module_00/ex00/megaphone.cpp
@@ -6,12 +6,24 @@
 /*   By: auzun <auzun@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/30 18:44:21 by auzun             #+#    #+#             */
-/*   Updated: 2022/12/30 18:46:55 by auzun            ###   ########.fr       */
+/*   Updated: 2023/01/02 02:01:40 by auzun            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 
-int	main(void)
+int	main(int ac, char **av)
 {
+	if (ac == 1)
+	{
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+		return (1);
+	}
+	for (int i = 1; i < ac; i++)
+	{
+		for (int j = 0; av[i][j]; j++)
+			std::cout << (char)toupper(av[i][j]);
+	}
+	std::cout << std::endl;
+	return (0);
 }
