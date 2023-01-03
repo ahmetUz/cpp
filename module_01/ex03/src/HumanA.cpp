93b7e0274402045b5cdc6438b7ce3e108a8c43ff
ahmetuz <pro.ahmetuzn@gmail.com>
2023-01-03 21:32:08 +0100

    ex03 done

--- module_01/ex03/src/HumanA.cpp
+++ module_01/ex03/src/HumanA.cpp
@@ -6,16 +6,14 @@
 /*   By: auzun <auzun@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/03 10:31:07 by auzun             #+#    #+#             */
-/*   Updated: 2023/01/03 10:54:53 by auzun            ###   ########.fr       */
+/*   Updated: 2023/01/03 21:13:09 by auzun            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "HumanA.hpp"
 
-HumanA::HumanA(std::string name, Weapon& Weapon)
+HumanA::HumanA(std::string name, Weapon& Weapon) : _name(name),_weapon(Weapon)
 {
-	this->_name = name;
-	this->_weapon = Weapon;
 }
 
 HumanA::~HumanA()
@@ -24,7 +22,7 @@ HumanA::~HumanA()
 
 void	HumanA::attack(void) const
 {
-	if (!this->_weapon.getType().empty)
+	if (this->_weapon.getType() != "")
 		std::cout << this->_name << " attacks with their " << this->_weapon.getType() << std::endl;
 	else
 		std::cout << this->_name << " doesn't have a weapon to attack." << std::endl;
--- module_01/ex03/src/HumanB.cpp
+++ module_01/ex03/src/HumanB.cpp
@@ -6,13 +6,13 @@
 /*   By: auzun <auzun@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/03 10:31:07 by auzun             #+#    #+#             */
-/*   Updated: 2023/01/03 10:48:28 by auzun            ###   ########.fr       */
+/*   Updated: 2023/01/03 21:19:28 by auzun            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "HumanB.hpp"
 
-HumanB::HumanB(std::string name)
+HumanB::HumanB(std::string name) : _name(name)
 {
 	this->_weapon = NULL;
 }
@@ -23,13 +23,13 @@ HumanB::~HumanB()
 
 void	HumanB::setWeapon(Weapon& weapon)
 {
-	this->_weapon = *weapon;
+	this->_weapon = &weapon;
 }
 
 void	HumanB::attack(void) const
 {
-	if (this->_weapon && !this->_weapon.getType().empty)
-		std::cout << this->_name << " attacks with their " << this->_weapon.getType() << std::endl;
+	if (this->_weapon && this->_weapon->getType() != "")
+		std::cout << this->_name << " attacks with their " << this->_weapon->getType() << std::endl;
 	else
 		std::cout << this->_name << " doesn't have a weapon to attack." << std::endl;
 }
